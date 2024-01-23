a8aade5c99110b4398fdc37ac5bec96a980238ba
andras.pulai <andras.pulai@gmail.com>
2024-01-23 13:54:38 +0100

    Merge remote-tracking branch 'origin/main'

--- msdp/msdp.py
+++ msdp/msdp.py
@@ -6,14 +6,14 @@ import time
 # This call will disable invalid https certificate   disable_warnings
 requests.urllib3.disable_warnings()
 
-#nbu_api_key = "A82V50vDyuSMjBzPULKXZH2x5kIxbIxg1IpL1q78R7jhoCm5wKWR5joAX-MgwM1k"
+# nbu_api_key = "A82V50vDyuSMjBzPULKXZH2x5kIxbIxg1IpL1q78R7jhoCm5wKWR5joAX-MgwM1k"
 nbu_api_hostname = "nbuprimary.lab.local"
 nbu_api_baseurl = "https://" + nbu_api_hostname + ":1556/netbackup"
 
 file1 = open('apikey.txt', 'r')
-nbu_api_key = file1.readline()
+nbu_api_key = file1.readline().strip()
+file1.close()
 print(nbu_api_key)
-
 
 nbu_api_content_type_v6 = "application/vnd.netbackup+json;version=6.0"
 nbu_api_content_type_v5 = "application/vnd.netbackup+json;version=5.0;charset=UTF-8"
@@ -56,96 +56,62 @@ t_now = datetime.now()
 t_30_days_ago = t_now - timedelta(days=30)
 print(t_now.strftime('%Y-%m-%dT%H:%M:%SZ'), t_30_days_ago.strftime('%Y-%m-%dT%H:%M:%SZ'))
 
-
 params = {
     'page[limit]': 10,
     'page[offset]': 0,
-    #'filter': "(severity eq 'CRITICAL') or (severity eq 'NOTICE')"
-    #'filter': "id gt 831"
-    #'filter': "createdDateTime gt 2022-02-14T00:00:00.000Z"
+    # 'filter': "(severity eq 'CRITICAL') or (severity eq 'NOTICE')"
+    # 'filter': "id gt 831"
+    # 'filter': "createdDateTime gt 2022-02-14T00:00:00.000Z"
     # not good 'filter': "createdDateTime gt 2022-02-14 01:33:00"
-    }
+}
 
-while True:
-    print("GET msdp server entries (page:",params['page[offset]'],") ...", end=" ")
-    response = requests.get(nbu_api_baseurl +
-                            "/config/servers/msdp-servers",
-                            params=params,
-                            verify=False,
-                            headers=header_v6)
+print("GET msdp server entries (page:", params['page[offset]'], ") ...", end=" ")
+response = requests.get(nbu_api_baseurl +
+                        "/config/servers/msdp-servers",
+                        params=params,
+                        verify=False,
+                        headers=header_v6)
 
-    print("done:", response.status_code)
-    if response.status_code != 200:
-        print("Error:", response)
-        quit(1)
+print("done:", response.status_code)
+if response.status_code != 200:
+    print("Error:", response)
+    quit(1)
 
-    parsed1=response.json()
-    # print(json.dumps(parsed1, indent=4, sort_keys=True))
-    # print(type(response), type(parsed1))
-    print_dict_path('',parsed1)
+parsed1 = response.json()
+# print(json.dumps(parsed1, indent=4, sort_keys=True))
+# print(type(response), type(parsed1))
+print_dict_path('', parsed1)
 
-    offset = parsed1['meta']['pagination']['offset']
+# print msdpserver data on screen
+for idx, item in enumerate(parsed1['data']):
+    print("Index\t\t:", idx)
+    print("Type\t\t:", item['type'])
+    print("ID\t\t:", item['id'])
+    print("lastupdated\t:", item['attributes']['lastUpdateTimeUTC'])
+    print("certHash\t:", item['attributes']['certHash'])
+    print("lastUpdatedDateTime\t:", item['attributes']['lastUpdatedDateTime'])
+    print()
 
-    # print catalog data on screen
-    for idx, item in enumerate(parsed1['data']):
-        print("Index\t\t:", idx)
-        print("Type\t\t:",item['type'])
-        print("ID\t\t:",item['id'])
-        print("lastupdated\t:",item['attributes']['lastUpdateTimeUTC'])
-        print("certHash\t:",item['attributes']['certHash'])
-        print("lastUpdatedDateTime\t:",item['attributes']['lastUpdatedDateTime'])
-        print()
+mystr = input("Enter index: ")
 
-    mystr = input("(n for next, p for prev, l for last, f for first q for quit): ")
-    if mystr == 'q':
-        exit(-1)
-    if mystr == 'n':
-        try:
-            params['page[offset]'] = parsed1['meta']['pagination']['next']
-            continue
-        except KeyError:
-            print("Error redoing query")
-            continue
-    if mystr == 'p':
-        try:
-            params['page[offset]'] = parsed1['meta']['pagination']['prev']
-            continue
-        except KeyError:
-            print("Error redoing query")
-            continue
-    if mystr == 'f':
-        try:
-            params['page[offset]'] = parsed1['meta']['pagination']['first']
-            continue
-        except KeyError:
-            print("Error redoing query")
-            continue
-    if mystr == 'l':
-        try:
-            params['page[offset]'] = parsed1['meta']['pagination']['last']
-            continue
-        except KeyError:
-            print("Error redoing query")
-            continue
+idx = int(mystr)
+msdpname = parsed1['data'][idx]['id']
 
-    idx=int(mystr)
-    msdpname=parsed1['data'][idx]['id']
+print(f"GET msdp server {msdpname} details ...", end=" ")
+response = requests.get(nbu_api_baseurl +
+                        f"/config/servers/msdp-servers/{msdpname}",
+                        params=params,
+                        verify=False,
+                        headers=header_v6)
 
-    print("GET msdp server details (page:", params['page[offset]'], ") ...", end=" ")
-    response = requests.get(nbu_api_baseurl +
-                            f"/config/servers/msdp-servers/{msdpname}",
-                            params=params,
-                            verify=False,
-                            headers=header_v6)
+print("done:", response.status_code)
+if response.status_code != 200:
+    print("Error:", response)
+    quit(1)
 
-    print("done:", response.status_code)
-    if response.status_code != 200:
-        print("Error:", response)
-        quit(1)
-
-    parsed2 = response.json()
-    # print(json.dumps(parsed1, indent=4, sort_keys=True))
-    # print(type(response), type(parsed1))
-    print_dict_path('', parsed2)
+parsed2 = response.json()
+# print(json.dumps(parsed1, indent=4, sort_keys=True))
+# print(type(response), type(parsed1))
+print_dict_path('', parsed2)
 
 print("process finished")
