e0e84fd888766e03bca68f04700d93c34421a424
Pulai Andras <andras.pulai@gmail.com>
2024-08-27 17:18:08 +0200

    Added renameFile part

--- recovery/11_recovery.py
+++ recovery/11_recovery.py
@@ -6,62 +6,13 @@ from datetime import datetime, timedelta
 requests.urllib3.disable_warnings()
 
 nbu_api_key = "A8fmNjyB5DvvzAN1CnRWblMv160U1Vt5hs5P2fqj81pJh9224HRus-w9YuXnwSMa"
-nbu_api_hostname = "nbumaster.lab.local"
-nbu_api_baseurl = "https://" + nbu_api_hostname + ":1556/netbackup"
-
-nbu_api_content_type_v6 = "application/vnd.netbackup+json;version=6.0"
-nbu_api_content_type_v5 = "application/vnd.netbackup+json;version=5.0;charset=UTF-8"
-nbu_api_content_type_v3 = "application/vnd.netbackup+json;version=3.0"
-
-header_v6 = {'Accept': nbu_api_content_type_v6,
-             'Authorization': nbu_api_key,
-             'Content-Type': nbu_api_content_type_v6}
-
-header_v5 = {'Accept': nbu_api_content_type_v5,
-             'Authorization': nbu_api_key,
-             'Content-Type': nbu_api_content_type_v5}
-
-header_v3 = {'Accept': nbu_api_content_type_v3,
-             'Authorization': nbu_api_key,
-             'Content-Type': nbu_api_content_type_v3}
-
-
-# This helper function will print the parsed json (dictionary)
-# in an easy to use format.
-# After a REST query is completed, You have to select the interesting fields
-# The aim of this function is to help you with that.
-# INPUT: The parsed response from a request (dict or list)
-# OUTPUT: objects with full path and values are printed on STDOUT
-# RETURN: none
-
-def print_dict_path(path, obj):
-    if isinstance(obj, dict):
-        for k, v in obj.items():
-            print_dict_path(path + "['" + k + "']", v)
-    elif isinstance(obj, list):
-        for idx, item in enumerate(obj):
-            print_dict_path(path + '[' + str(idx) + ']', item)
-    else:
-        print(path + " =>", obj)
-
-
-import json
-import requests
-from datetime import datetime, timedelta
-
-# This call will disable invalid https certificate disable_warnings
-requests.urllib3.disable_warnings()
-
-nbu_api_key = "A8fmNjyB5DvvzAN1CnRWblMv160U1Vt5hs5P2fqj81pJh9224HRus-w9YuXnwSMa"
-nbu_api_hostname = "nbumaster.lab.local"
+nbu_api_hostname = "nbuprimary.lab.local"
 nbu_api_baseurl = "https://" + nbu_api_hostname + ":1556/netbackup"
 asset_id = "84ba26e1-bd89-45c6-9ad4-196b0d8f9287"
 
 nbu_api_content_type_v6 = "application/vnd.netbackup+json;version=6.0"
 nbu_api_content_type_v5 = "application/vnd.netbackup+json;version=5.0;charset=UTF-8"
 nbu_api_content_type_v3 = "application/vnd.netbackup+json;version=3.0"
-
-nbu_api_content_type_multipartform = v5 = "multipart/vnd.netbackup+form-data;version=5.0"
 
 header_v6 = {'Accept': nbu_api_content_type_v6,
              'Authorization': nbu_api_key,
@@ -127,10 +78,19 @@ mySelectionsFile = [
     }
 ]
 
+myRenameFile = [
+    {
+        "from": "/etc/hosts",
+        "to": "/tmp/hosts_file_restored"
+    }
+]
+
 multipart_form_data = {
-    "recoveryRequest": (blob, myRecoveryRequest),
-    "selectionsFile": (blob, mySelectionsFile)
+    "recoveryRequest": ("blob", json.dumps(myRecoveryRequest)),
+    "selectionsFile": ("blob", json.dumps(mySelectionsFile)),
+    "renameFile": ("blob", json.dumps(myRenameFile))
 }
+
 print("POST to start restore ...", end=" ")
 response = requests.post(nbu_api_baseurl +
                          "/recovery/workloads/physical/scenarios/granular-files-folders/recover",
